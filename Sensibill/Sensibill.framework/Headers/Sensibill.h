a0c6413074ab44f9d66544247df195952b1d441e
Kiril Strax <kiril@getsensibill.com>
2023-07-26 16:38:22 -0400

    Release 2023.0.1

---
+++ Sensibill/Sensibill.framework/Headers/Sensibill.h
@@ -0,0 +1,15 @@
+//
+//  Sensibill.h
+//  Sensibill
+//
+//  Created by Sensibill on 2018-04-19.
+//  Copyright © 2018 Sensibill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sensibill.
+FOUNDATION_EXPORT double SensibillVersionNumber;
+
+//! Project version string for Sensibill.
+FOUNDATION_EXPORT const unsigned char SensibillVersionString[];
